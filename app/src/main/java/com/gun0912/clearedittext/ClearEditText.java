7f87c934a8efffe273b39375ff7fb86f79914023
gun0912 <gun0912@selphone.co.kr>
2016-04-12 01:11:26 +0900

    Initial commit

---
+++ app/src/main/java/com/gun0912/clearedittext/ClearEditText.java
@@ -0,0 +1,119 @@
+package com.gun0912.clearedittext;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+/**
+ * Created by TedPark on 16. 4. 11..
+ */
+
+public class ClearEditText extends AppCompatEditText implements TextWatcher, View.OnTouchListener, View.OnFocusChangeListener {
+
+    private Drawable clearDrawable;
+    private OnFocusChangeListener onFocusChangeListener;
+    private OnTouchListener onTouchListener;
+
+    public ClearEditText(final Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearEditText(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearEditText(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener onFocusChangeListener) {
+        this.onFocusChangeListener = onFocusChangeListener;
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener onTouchListener) {
+        this.onTouchListener = onTouchListener;
+    }
+
+
+    private void init() {
+
+        clearDrawable = ContextCompat.getDrawable(getContext(), R.drawable.abc_ic_clear_mtrl_alpha);
+        DrawableCompat.setTint(clearDrawable, getCurrentHintTextColor());
+        clearDrawable.setBounds(0, 0, clearDrawable.getIntrinsicHeight(), clearDrawable.getIntrinsicHeight());
+        setClearIconVisible(false);
+
+
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+
+
+    @Override
+    public void onFocusChange(final View view, final boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(getText().length() > 0);
+        } else {
+            setClearIconVisible(false);
+        }
+
+        if (onFocusChangeListener != null) {
+            onFocusChangeListener.onFocusChange(view, hasFocus);
+        }
+    }
+
+
+    @Override
+    public boolean onTouch(final View view, final MotionEvent motionEvent) {
+        final int x = (int) motionEvent.getX();
+        if (clearDrawable.isVisible() && x > getWidth() - getPaddingRight() - clearDrawable.getIntrinsicWidth()) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                setText(null);
+            }
+            return true;
+        }
+
+        if (onTouchListener != null) {
+            return onTouchListener.onTouch(view, motionEvent);
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public final void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
+        if (isFocused()) {
+            setClearIconVisible(s.length() > 0);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+
+    private void setClearIconVisible(boolean visible) {
+        clearDrawable.setVisible(visible, false);
+        setCompoundDrawables(null, null, visible ? clearDrawable : null, null);
+    }
+
+
+}
